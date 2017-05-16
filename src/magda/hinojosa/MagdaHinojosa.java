d4c45ebf5b166e107ef2dcb0c412638f9fca34c3
hinojosa07 <magdha.1307@hotmail.com>
2017-05-15 19:44:20 -0500

    Update MagdaHinojosa.java

--- src/magda/hinojosa/MagdaHinojosa.java
+++ src/magda/hinojosa/MagdaHinojosa.java
@@ -16,7 +16,7 @@ public class MagdaHinojosa {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo cruel");
         
     }
     
